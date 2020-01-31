d42b393b76a91f1809ad543d6f8d360b2ee8b2e7
Dan Kim <pluvios@pluvios.com>
2020-02-01 04:08:39 +0900

    Yeah did something about array and object and variable data types

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ let a = 2;
 let b = "a";
 let c = a + "+" + b;
 console.log(c,a,b);
+//to do : finish this
+const what = "Dan";
+const wat = false;
+const whut = 777;
+const whit = 55.7;
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+
+console.log(mon, tue, wed, thu, fri);
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek);
+
+const meInfo = {
+    Name: "dan",
+    Age : "35",
+    Birth: "0219",
+    Occupation: "CEO",
+    Home: "Seoul"
+};
+
+console.log(meInfo.Age);
+
