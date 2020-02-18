f0b32941fc2dc37c1b0e47ae3b25448a23ce47d8
Dan Kim <pluvios@pluvios.com>
2020-02-19 04:17:22 +0900

    some tests on eventlistener loadend

--- bg.js
+++ bg.js
@@ -5,16 +5,43 @@ const body = document.querySelector("body");
 const IMG_NUMBER = 5;
 
 
-function handleImgLoad(){
-    console.log("finished loading");
+function handleImageLoad(event){
+    if(event === "loadstart"){
+        console.log("image load start");
+    } else if(event === "load"){
+        console.log("image load");
+    } else if(event === "loadend"){
+        console.log("image load end");
+    } else if(event === "progress"){
+        console.log("image load progress");
+    } else if(event === "error"){
+        console.log("image load error");
+    } else if(event === "abort"){
+        console.log("image load abort");
+    } 
+
 }
+
 
 function paintImage(imageNumber){
     const image = new Image();
     image.src = `images/${imageNumber}.jpg`;
-    image.addEventListener("loadend", handleImgLoad)
     body.appendChild(image);
-    image.classList.add("bgImage")
+    image.addEventListener("loadstart", handleImageLoad)
+    image.addEventListener("load", handleImageLoad)
+    image.addEventListener("loadend", handleImageLoad)
+    image.addEventListener("progress", handleImageLoad)
+    image.addEventListener("error", handleImageLoad)
+    image.addEventListener("abort", handleImageLoad)
+    image.classList.add("bgImage");
+
+    
+    /*function(image){
+            console.log("image loaded");
+            body.appendChild(image);
+            image.classList.add("bgImage");
+    });
+    */
 }
 
 function genRandom(){
